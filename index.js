2c27245233a0a56e629ea65a5c7a780ad7e6b9aa
antonio <antonio.rodriguez@glueops.dev>
2022-04-16 21:34:37 -0600

    fix: incorrect js syntax

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>GlueOps</h>");
+  res.send("<h1>GlueOps init!</h>");
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('listening on port ${port}'));
+app.listen(port, () => console.log(`listening on port ${port}`));
 
