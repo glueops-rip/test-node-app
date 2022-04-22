1825d5bf7d2a8b7656949b3308fab0ab4b73146c
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-22 21:06:30 +0000

    debugging

--- cloud_env.sh
+++ cloud_env.sh
@@ -1,3 +1,11 @@
 while IFS='=' read -r -d '' n v; do
     printf '  %s: "%s"\n' "$n" "$v" >> app.yaml
 done < <(env -0)
+
+
+sed -i '/^  HOSTNAME:/d' app.yaml 
+sed -i '/^  _:/d' app.yaml 
+sed -i '/^  BUILDER_OUTPUT:/d' app.yaml 
+sed -i '/^  PWD:/d' app.yaml 
+sed -i '/^  HOME:/d' app.yaml 
+sed -i '/^  SHLVL:/d' app.yaml 
