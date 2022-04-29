ef9426ada87d46e438ccffe79127355a66699bb1
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-29 22:36:43 +0000

    deugging

--- cloud_env.sh
+++ cloud_env.sh
@@ -2,6 +2,8 @@ while IFS='=' read -r -d '' n v; do
     printf '  %s: "%s"\n' "$n" "$v" >> app.yaml
 done < <(env -0)
 
+echo "BEFORE REMOVALS: \n\n"
+cat app.yaml
 
 sed -i '/^  HOSTNAME:/d' app.yaml 
 sed -i '/^  _:/d' app.yaml 
@@ -11,5 +13,6 @@ sed -i '/^  HOME:/d' app.yaml
 sed -i '/^  SHLVL:/d' app.yaml 
 sed -i '/^  PATH:/d' app.yaml 
 
+echo "AFTER REMOVALS: \n\n"
 
 cat app.yaml
