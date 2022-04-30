ead60f38a3562b00e38047cf25b73cac50798c00
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-30 03:59:54 +0000

    revert commit: cloud_env.sh

--- cloud_env.sh
+++ cloud_env.sh
@@ -1,9 +1,14 @@
 
+echo "ALL ENV VARIABLES:"
+env
+
 while IFS='=' read -r -d '' n v; do
     printf '  %s: "%s"\n' "$n" "$v" >> app.yaml
 done < <(env -0)
 
 
+echo "BEFORE REMOVALS: \n\n"
+cat app.yaml
 
 sed -i '/^  HOSTNAME:/d' app.yaml 
 sed -i '/^  _:/d' app.yaml 
@@ -12,6 +17,7 @@ sed -i '/^  PWD:/d' app.yaml
 sed -i '/^  HOME:/d' app.yaml 
 sed -i '/^  SHLVL:/d' app.yaml 
 sed -i '/^  PATH:/d' app.yaml 
-sed -i '/^  XTERM:/d' app.yaml 
-sed -i '/^  LS_COLORS:/d' app.yaml 
 
+echo "AFTER REMOVALS: \n\n"
+
+cat app.yaml
