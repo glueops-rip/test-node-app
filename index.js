457187d91b561aec2e7d854423e5fa75589f7d32
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-30 15:03:08 +0000

    feat: adding shared secret env var

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 
 const app = express();
-const username = process.env.USERNAME || "example-username";
-const password = process.env.PASSWORD || "example-password";
-const connectionString = process.env.CONNECTION_STRING;
+const username = process.env.USERNAME || "default-username";
+const password = process.env.PASSWORD || "default-password";
+const connectionString = process.env.CONNECTION_STRING || "default-connection-string";
+const SHARED_SECRET = process.env.SHARED_SECRET || "default-shared-secret";
 
 
 
 
 app.get("/", (req, res) => {
-  res.send("<h1>GlueOps init!</h1><h2><br /> Username: " + username + "<br /> Password: " + password + "<br /> Connection String: " + connectionString + "</h2>");
+  res.send("<h1>GlueOps init!</h1><h2><br /> Username: " + username + "<br /> Password: " + password + "<br /> Connection String: " + connectionString + "<br />  Shared Secret: " + SHARED_SECRET  + "</h2>");
 });
 
 const port = process.env.PORT || 3000;
